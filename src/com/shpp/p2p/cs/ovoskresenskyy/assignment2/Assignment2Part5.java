7cf3b1c9f2e12b1594f88b43fa8736ae6e6337ac
ovoskresenskyi <7949894@ukr.net>
2023-05-13 01:21:55 +0300

    Code cleaning. Adding comments

--- src/com/shpp/p2p/cs/ovoskresenskyy/assignment2/Assignment2Part5.java
+++ src/com/shpp/p2p/cs/ovoskresenskyy/assignment2/Assignment2Part5.java
@@ -5,11 +5,17 @@ import com.shpp.cs.a.graphics.WindowProgram;
 
 import java.awt.*;
 
+/**
+ * Another optical illusion.
+ * <p>
+ * The program draw a matrix of black boxes separated by "streets".
+ * Drawn boxes are centered in the window.
+ */
 public class Assignment2Part5 extends WindowProgram {
 
     /* The number of rows and columns in the grid, respectively. */
     private static final int NUM_ROWS = 5;
-    private static final int NUM_COLS = 6;
+    private static final int NUM_COLS = 7;
 
     /* The width and height of each box. */
     private static final double BOX_SIZE = 50;
@@ -20,17 +26,26 @@ public class Assignment2Part5 extends WindowProgram {
     @Override
     public void run() {
 
+        /* The position of each box relative to the upper-left corner of the previous one. */
         double offset = BOX_SIZE + BOX_SPACING;
-        double left = (getWidth() - offset * NUM_COLS) / 2;
-        double top = (getHeight() - offset * NUM_ROWS) / 2;
+        /* The x coordinate of the upper-left corner of the first box. */
+        double x = (getWidth() - offset * NUM_COLS) / 2;
+        /* The y coordinate of the upper-left corner of the first box. */
+        double y = (getHeight() - offset * NUM_ROWS) / 2;
 
         for (int i = 0; i < NUM_COLS; i++) {
             for (int j = 0; j < NUM_ROWS; j++) {
-                drawBox(left + offset * i, top + offset * j);
+                drawBox(x + offset * i, y + offset * j);
             }
         }
     }
 
+    /**
+     * The method draws rectangle according to the received parameters
+     *
+     * @param x - The x coordinate of the upper-left corner of the box.
+     * @param y - The y coordinate of the upper-left corner of the box.
+     */
     private void drawBox(double x, double y) {
         GRect rectangle = new GRect(x, y, BOX_SIZE, BOX_SIZE);
         rectangle.setFilled(true);
