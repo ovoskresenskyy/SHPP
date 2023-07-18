ef47595fe1109ee885aa25dd44899cde1ef6a2a3
ovoskresenskiy <7949894@ukr.net>
2023-07-18 13:12:36 +0200

    SHPP Assignment10Part1 Enums

---
+++ src/com/shpp/p2p/cs/ovoskresenskyy/assignment10/enums/Operator.java
@@ -0,0 +1,90 @@
+package com.shpp.p2p.cs.ovoskresenskyy.assignment10.enums;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * This class is representing of all operators which are supported in our calculator.
+ */
+public enum Operator {
+
+    EXPONENT('^', 1),
+    MULTIPLICATION('*', 2),
+    DIVISION('/', 3),
+    SUBTRACTION('-', 4),
+    ADDITION('+', 5);
+
+    /**
+     * Text representing of the operator
+     */
+    final private char symbol;
+
+    /**
+     * Priority of the execution inside the expression.
+     * ^ >> * / >> + -
+     */
+    final private int priority;
+
+    /**
+     * Comparator, to be sure that operator held in the correct order.
+     */
+    public static final Comparator<Operator> priorityComparator = Comparator.comparingInt(o -> o.priority);
+
+    /**
+     * Simple collection to use it instead of values[]
+     */
+    private static final Map<Character, Operator> BY_SYMBOL = new HashMap<>();
+
+    /* Used static init area to fill the collection. */
+    static {
+        for (Operator e : values()) {
+            BY_SYMBOL.put(e.symbol, e);
+        }
+    }
+
+    Operator(Character symbol, int priority) {
+        this.symbol = symbol;
+        this.priority = priority;
+    }
+
+    /**
+     * This method is responsible for determination is received symbol can be matched
+     * with any supported operator.
+     *
+     * @param symbol - Char to be checked if it's operator
+     * @return True if received character is operator, false if not.
+     */
+    public static boolean isOperator(Character symbol) {
+        return BY_SYMBOL.containsKey(symbol);
+    }
+
+    /**
+     * This method is responsible for finding the operator by the given character.
+     *
+     * @param symbol - Character to find matched operator into the Map
+     * @return Operator if it's found by the received character, null if not.
+     */
+    public static Operator getOperatorBySymbol(Character symbol) {
+        return BY_SYMBOL.get(symbol);
+    }
+
+    /**
+     * This method is responsible for creating the Regex from the all
+     * supported operators.
+     *
+     * @return Something like that [\\+\\-\\* ...] according to the supported operators.
+     */
+    public static String getOperatorsAsDelimiter() {
+        return BY_SYMBOL.values().stream()
+                .map(operator -> operator.symbol)
+                .map(Object::toString)
+                .collect(Collectors.joining("\\", "[", "]"));
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(symbol);
+    }
+}
---
+++ src/com/shpp/p2p/cs/ovoskresenskyy/assignment10/enums/ParsingError.java
@@ -0,0 +1,40 @@
+package com.shpp.p2p.cs.ovoskresenskyy.assignment10.enums;
+
+/**
+ * This class is hold all known errors
+ */
+public enum ParsingError {
+    ARGS_EMPTY("There are no any arguments present!"),
+
+    /**
+     * Text expression errors
+     */
+    OPERANDS_NOT_VALID("Expression contains forbidden symbols!"),
+    VARIABLE_PRESENT("Expression contains don't matched variables!"),
+    EMPTY_OPERANDS_PRESENT("Number of operators doesn't match with the given operands!"),
+
+    /**
+     * Parsed expression errors
+     */
+    OPERANDS_NOT_PRESENT("There are no operands present in the expression!"),
+    OPERATORS_NOT_PRESENT("There are no operators present in the expression!"),
+
+    NO_ERROR("");
+
+    private final String errorMessage;
+
+    ParsingError(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    /**
+     * This method is simply prints the error message to the user
+     *
+     * @param error - Parsing error to be printed
+     */
+    public static void showErrorMessage(ParsingError error) {
+        if (error != NO_ERROR) {
+            System.out.println(error.errorMessage);
+        }
+    }
+}
